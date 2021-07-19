ea1805fa705bf2f680c642daaa4ddc0e56473669
Peter Treese <ptreese@PeteTs-MBP.fios-router.home>
2021-07-19 16:45:38 -0400

    Working version of Movie search, need css work

---
+++ src/components/Movie.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, overview, vote_average, genre_ids }) => {
+  const setVoteClass = (vote) => {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "yellow";
+    } else {
+      return "red";
+    }
+  };
+
+  return (
+    <div className="movie">
+      {poster_path ? (
+        <img src={IMAGE_API + poster_path} alt={title} />
+      ) : (
+        <img src="no-cover.png" alt={title} />
+      )}
+      <div className="movie-info">
+        <h3>{title}</h3>
+    
+        <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+      </div>
+
+      <div className="movie-overview">
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
